26bf85cfcdd5130732f90789d4748420ab1cd9de
Dragos Nedelcu <dragosgn@gmail.com>
2019-02-16 11:11:05 +0100

    fixe

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: "one"
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          Welcome to Kleen.io
-        </header>
+        {
+          (this.state.step = "one" && (
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              Welcome to Kleen.io
+            </header>
+          ))
+        }
       </div>
     );
   }
